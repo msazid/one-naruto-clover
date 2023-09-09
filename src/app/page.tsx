5354234b2ede9ec2c647f0bc1f02f09eb1526592
im-mahadi <itz.me.mahadi@gmail.com>
2023-09-09 16:24:59 +0600

    add dark mode theme functionality ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import ThemeToggle from '@/components/global/ThemeToggle';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='flex items-center gap-2'>
+      Click the button to change the theme
+      <ThemeToggle />
+    </div>
+  );
 }
