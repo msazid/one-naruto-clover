2d6e6b54229ecd5d97b220300294adf4c0494d5e
msazid <mdsaziduli14@gmail.com>
2023-09-10 22:20:24 +0600

    this is hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from '@/components/global/ThemeToggle';
 export default function Home() {
   return (
     <div className='flex items-center gap-2'>
-      Click the button to change the theme
+      <p>Hola top</p>
       <ThemeToggle />
     </div>
   );
